231b817f3575b4fbf90bb0e1ff09425feb0155ad
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-11-27 12:13:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt update && apt upgrade -y
 
 echo -e "\nauto usb0\nallow-hotplug usb0\niface usb0 inet static\n\taddress 1.0.0.1\n\tnetmask 0.0.0.0" >> /etc/network/interfaces
 echo "dtoverlay=dwc2" >> /boot/config.txt
@@ -7,5 +6,5 @@ echo -e "dwc2\ng_ether" >> /etc/modules
 echo "/bin/sh /home/pi/poisontap/pi_startup.sh" >> /etc/rc.local
 mkdir /home/pi/poisontap
 chown -R pi /home/pi/poisontap
-apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade -y
 apt-get -y install isc-dhcp-server dsniff screen nodejs
